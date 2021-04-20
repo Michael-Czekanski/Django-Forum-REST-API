ce399ad9d46981ff9c6ab810054463944f86ed6d
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-20 21:52:31 +0200

    Add users and forum apps to installed apps in settings.py

--- forum_restapi/settings.py
+++ forum_restapi/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'forum.apps.ForumConfig',
     'rest-framework',
     'django.contrib.admin',
     'django.contrib.auth',
