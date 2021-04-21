5a19da82ae20b23a60c3930d1909a846edb2edc2
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-21 17:28:33 +0200

    "Close #2 Add simple users list view in users app"

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import User
+from .serializers import UserSerializer
+
 
 # Create your views here.
+
+def users_list(request):
+    """
+    List all users.
+    """
+
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return JsonResponse(serializer.data, safe=False)
