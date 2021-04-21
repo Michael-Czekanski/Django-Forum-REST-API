31770fcacdd8fe708c21e043493157a5214e9633
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-21 17:38:36 +0200

    "#3 Add Topic model class in forum app"

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from users.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    creation_date = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
