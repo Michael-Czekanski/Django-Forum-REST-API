0a49f401df56c7c71a230ae4df9adfc339e213e8
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-22 14:42:17 +0200

    Add __str__ method in Topic model cls

--- forum/models.py
+++ forum/models.py
@@ -8,3 +8,6 @@ class Topic(models.Model):
     name = models.CharField(max_length=255)
     creation_date = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.name} {self.created_by.username} {self.creation_date}"
