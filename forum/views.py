698451cd88e23407bf960b4cfd4e40d3d3de20e5
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-22 14:59:14 +0200

    Add simple view displaying all topics in forum app.

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Topic
+from django.http import JsonResponse
+from .serializers import TopicSerializer
+
 
 # Create your views here.
+
+def topics_list(request):
+    """
+    List of all topics.
+    """
+
+    topics = Topic.objects.all()
+    serializer = TopicSerializer(topics, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
