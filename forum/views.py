188f35848485fe839a48020b83ae9f51589cbf20
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-24 01:24:49 +0200

    "#5 Add topic details view"

--- forum/views.py
+++ forum/views.py
@@ -24,3 +24,26 @@ def topics_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def topic_detail(request, pk):
+    try:
+        topic = Topic.objects.get(pk=pk)
+    except Topic.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TopicSerializer(topic)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TopicSerializer(instance=topic, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        topic.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
