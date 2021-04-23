d06aa00e174e9acdec20f9b8093a2d70a167cfbe
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-24 01:25:22 +0200

    "Close #5. Add path to topic details view"

--- forum/urls.py
+++ forum/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import topics_list
+from .views import topics_list, topic_detail
 
 urlpatterns = [
-    path('topics/', topics_list, name='forum-topics')
+    path('topics/', topics_list, name='forum-topics'),
+    path('topics/<int:pk>/', topic_detail, name='forum-topic-detail')
 ]
