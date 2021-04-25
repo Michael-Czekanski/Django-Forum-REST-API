84bbb2ac54e00056071fefd6a8a1d5f5cbfd1ee5
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-25 14:04:51 +0200

    "Close #7. Only authenticated users can view details of other users"

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from .models import User
 from .serializers import UserSerializer, UserDetailSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import permissions
 
 
 class UserList(APIView):
@@ -17,6 +18,7 @@ class UserList(APIView):
 
 
 class UserDetail(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, pk):
         """
